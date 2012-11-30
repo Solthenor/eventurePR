f6045a8cc9230413a7c4b1879854e81bfdc86805
Kevin Hernandez <kevin.hernandez2@upr.edu>
2012-11-30 17:13:35 -0400

    tra

--- contacts.php
+++ contacts.php
@@ -38,10 +38,8 @@ if (isset($_POST['submit'])) {
     $stmt->execute();
 
     $result = $stmt->fetchAll();
-
-
-
  }
+
 
 ?>
 <!DOCTYPE html>
@@ -144,9 +142,13 @@ if (isset($_POST['submit'])) {
                                     <a class="wsite-button wsite-button-small wsite-button-normal" href="profile.php?userID=<?php echo $user['userID'] ?>" >
                                         <span class="wsite-button-inner">See profile</span>
                                     </a>
+                                    <form action="friendAdded.php" method="POST">
                                     <a class="wsite-button wsite-button-small wsite-button-normal" href="profile.php?userID=<?php echo $user['userID'] ?>" >
-                                        <span class="wsite-button-inner">Add as friend</span>
+                                        <input type='submit' name="addFriend" value="Add Friend" class="wsite-button-inner" />
+
                                     </a>
+                                    </form>
+
                                 </td>
 
                             </tr>
---
+++ friendAdded.php
@@ -0,0 +1,120 @@
+<?php
+require_once('db.php');
+require_once('mobileRedirect.php');
+require_once('logoutHandler.php');
+require_once('checkAuth.php');
+
+$userID = $_GET['userID'];
+$friendID = $_GET['addFriend'];
+
+// Checks if user is logged in, otherwise returns index
+if (!$loggedin && !isset($userID)) {
+    header('Location: index.php');
+    return;
+}
+
+if(isset($userID)) {
+    $id = $userID;
+}
+
+
+if (isset($_POST['addFriend'])) {
+    $db = db::getInstance();
+    $sql = "INSERT INTO AddFriend
+           SET
+               userID1 = '{$id}',
+               userID2 = '{$friendID}';";
+
+
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+
+    $sql2 = "INSERT INTO AddFriend
+           SET
+               userID2 = '{$id}',
+               userID1 = '{$friendID}';";
+
+
+    $stmt2 = $db->prepare($sql2);
+    $stmt2->execute();
+
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>E-venturePR - My Contacts</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    <link rel='stylesheet' type='text/css' href='http://cdn1.editmysite.com/editor/libraries/fancybox/fancybox.css?1346362758'>
+    <link rel='stylesheet' href='http://cdn1.editmysite.com/editor/images/common/common-v2.css?buildTime=1346362758' type='text/css' />
+    <link rel='stylesheet' type='text/css' href='css/main_style.css' title='wsite-theme-css' />
+    <link href='http://cdn1.editmysite.com/editor/fonts/Capture_it/font.css?2' rel='stylesheet' type='text/css' />
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style type='text/css'>
+        #wsite-content div.paragraph, #wsite-content p, #wsite-content .product-block .product-title, #wsite-content .product-description, .blog-sidebar div.paragraph, .blog-sidebar p, .wsite-form-field label, .wsite-form-field label {}
+        #wsite-content h2, #wsite-content .product-long .product-title, #wsite-content .product-large .product-title, #wsite-content .product-small .product-title, .blog-sidebar h2 {}
+        #wsite-title{font-family:'Capture it' !important;color:#009900 !important;}
+        #wsite-content a:link, .blog-sidebar a:link{color:#FFFFFF }
+        #wsite-content a:visited, .blog-sidebar a:visited{color:#FFFFFF }
+        #wsite-content a:hover, .blog-sidebar a:hover{color:#FFFFFF }
+    </style>
+    <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js'></script>
+    <script type='text/javascript' src='http://cdn1.editmysite.com/editor/libraries/jquery_effects.js?1346362758'></script>
+    <script type='text/javascript' src='http://cdn1.editmysite.com/editor/libraries/fancybox/fancybox.min.js?1346362758'></script>
+    <script type='text/javascript' src='http://cdn1.editmysite.com/editor/images/common/utilities-jq.js?1346362758'></script>
+    <script type='text/javascript' src='http://cdn1.editmysite.com/editor/libraries/flyout_menus_jq.js?1346362758'></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body class='wsite-theme-dark tall-header-page wsite-page-index'>
+<div id="wrapper">
+    <table id="header">
+        <tr>
+            <td id="logo"><span class='wsite-logo'><a href='/'><span id="wsite-title">E-venturePR</span></a></span></td>
+            <td id="header-right">
+                <table style="width: 150px;">
+                    <?php if($loggedin) { ?>
+                    <tr>
+                        <td class="phone-number"><span class='wsite-text'><a href="profile.php" style="color: #32CD32; text-decoration: underline; ">Profile</a> | 
+                               <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" style="float: right;">
+                                   <input type="hidden" value="logout" name="loggedOut" />
+                                   <input type="hidden" style="color: #32CD32; text-decoration: underline;" value="Log out" />
+                                   <a href="#" onclick="this.parentNode.submit()" style="color: #32CD32; text-decoration: underline; ">Logout</a>
+                               </form>
+                        </td>
+
+                        <td class="social"></td>
+                    </tr>
+
+                    <?php }  else {?>
+                    <tr>
+                        <td class="phone-number"><span class='wsite-text'>Don't have an account? Register <a href="login.php" style="color: #32CD32; text-decoration: underline; ">HERE</a> | <a href="login.php" style="color: #32CD32; text-decoration: underline;">Sign in</a></span></td>
+                        <td class="social"></td>
+                    </tr>
+                    <?php }?>
+                </table>
+                <div class="search"></div>
+            </td>
+        </tr>
+    </table>
+    <div id="navigation">
+        <ul><li id='active'><a href='index.php'>Home</a></li><li id='pg145650631833651339'><a href='events.php?category=Concert'>Music</a></li><li id='pg404778243583026952'><a href='events.php?category=Sports'>Sports</a></li><li id='pg441792526610757515'><a href='events.php?category=Entertainment'>Entertainment</a></li><li id='pg269210016325162137'><a href='events.php?category=Business'>Business & Education</a></li><li id="pgabout_us"><a href="about.php">About Us</a></li></ul>
+    </div>
+    <div id="container">
+        <div id="content">
+
+
+            <span style="line-height: 40px;">Your friend has been added.</span>
+
+            </div>
+
+            </div>
+        </div>
+        <div id="footer"></div>
+        <div class="clear"></div>
+    </div>
+
+</body>
+</html>
